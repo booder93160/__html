515c24d427e1c0c8bee97599835615b73b4658f1
booder93160 <157692581+booder93160@users.noreply.github.com>
2024-05-18 16:37:31 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+function req_db($req, $params)
+{
+    $db = new SQLite3("./truc.db");
+    $stmt = $db->prepare($req);
+    
+    // Liaison des paramètres
+    foreach ($params as $key => $value) {
+        $stmt->bindValue($key, $value);
+    }
+    
+    $results = $stmt->execute();
+    $toret = $results->fetchArray();
+    return $toret;
+}
+
+session_start();
+
+if (!empty($_POST["username"]) && !empty($_POST["password"]))
+{
+    // Récupération des valeurs des champs username et password
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
+    
+    // Utilisation de la requête préparée avec les paramètres
+    $a = req_db("SELECT * FROM users WHERE username = :username AND password = :password", [
+        ':username' => $username,
+        ':password' => $password
+    ]);
+
+    if ($a)
+    {
+        printf("Successfully connected. Redirecting...");
+        $_SESSION["CONNECTED"] = $a["admin"];
+        $_SESSION["USERNAME"] = $a["username"];
+        $session_id = bin2hex(random_bytes(32));
+        setcookie("JSESSID", $session_id, time() + 3600, "/", "", false, true);
+    }
+    else
+    {
+        printf("Failed connecting. Redirecting...");
+    }
+}else{
+    printf("Veuillez remplir tous les champs");
+}
+
+?>
+<script>
+    window.onload = function() { setTimeout(function() { window.location.href = "/"; }, 2000); }
+</script>
